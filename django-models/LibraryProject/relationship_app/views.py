f88ce0f3beaf55f2c32ca4fc8f7fc23dfa865d3f
BisaUmutoni <bclaudette636@gmail.com>
2024-08-23 00:14:15 +0200

    views/ Library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 from .views import list_books, LibraryDetailView
 from . views import list_books
 
+
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
