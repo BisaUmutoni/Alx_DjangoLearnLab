4731aa912959424c1578a486a6b4e0840b595458
BisaUmutoni <bclaudette636@gmail.com>
2024-08-23 00:08:49 +0200

    views List book

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,10 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import book_list, LibraryDetailView
+from . import views
+from .views import list_books, LibraryDetailView
+from . views import list_books
 
 urlpatterns = [
-    path('books/', book_list, name='book_list'),
+    path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from .models import Book
-from .models import Library
-
+from .models import Book, Library
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app\list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView():
     model = Library
-    template_name = 'relationship_app\library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
