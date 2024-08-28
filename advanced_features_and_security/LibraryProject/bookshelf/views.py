dda93831fa19e5b48570a970a991849e370df801
BisaUmutoni <bclaudette636@gmail.com>
2024-08-28 18:23:49 +0200

    work 1111

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm
 
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to the Library")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/edit_book.html', {'form': form})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
--- advanced_features_and_security/LibraryProject/relationship_app/forms.py
+++ advanced_features_and_security/LibraryProject/relationship_app/forms.py
@@ -1,22 +1,13 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from .models import UserProfile
+from .models import Book
 
-class UserProfileForm(UserCreationForm):  
+class BookForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username', 'password']
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-
-            # Save profile details
-            UserProfile.objects.create(
-                user=user,
-                password=user.password,
-            )
-        return user
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+        widgets = {
+            'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
