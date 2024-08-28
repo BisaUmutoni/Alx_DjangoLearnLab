7666dfdcf83ed6f33bba9fea539e90f7b9ea6844
BisaUmutoni <bclaudette636@gmail.com>
2024-08-29 00:49:33 +0200

    DRF done

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -12,3 +13,4 @@ class BookViewSet(viewsets.ModelViewSet):
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
