86dada563c47aff1d6fbbd183c98dec549369bec
BisaUmutoni <bclaudette636@gmail.com>
2024-09-17 01:25:35 +0200

    Authentification functionalities

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.urls import include
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from blog.views import register, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
+
+    
--- vd/Scripts/activate.bat
+++ vd/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Euro Scholars\OneDrive\Desktop\KWIGA\ALX\BE\Alx_DjangoLearnLab\advanced_api_project\vd
+set VIRTUAL_ENV=C:\Users\Euro Scholars\OneDrive\Desktop\KWIGA\ALX\BE\Alx_DjangoLearnLab\vd
 
 if not defined PROMPT set PROMPT=$P$G
 
