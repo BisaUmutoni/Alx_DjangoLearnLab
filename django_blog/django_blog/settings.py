a8040439517ad8f89e79f0ffbff1e3067f21d46e
BisaUmutoni <bclaudette636@gmail.com>
2024-09-16 11:16:47 +0200

    Django database configured

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":"django_blog",
+        "USER":"root",
+        "PASSWORD":"Maralba16!MYSQL",
+        "HOST":"localhost",
+        "PORT":"3306",
     }
 }
 
