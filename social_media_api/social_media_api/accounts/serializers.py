a1e3768ebc97e1e0282fefb960ebc5f7afa97e3a
BisaUmutoni <bclaudette636@gmail.com>
2024-09-21 10:35:12 +0200

    Serializers registration corrected

--- social_media_api/social_media_api/accounts/serializers.py
+++ social_media_api/social_media_api/accounts/serializers.py
@@ -1,7 +1,29 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
-class UserSerializer(serializers.ModelSerializer):
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        model = User
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        # Create a token for the new user
+        Token.objects.create(user=user)
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs['username'], password=attrs['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid username or password.")
+        return user
