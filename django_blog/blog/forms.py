f43be1c780e089510cfdb781842b5288dc9d5a5b
BisaUmutoni <bclaudette636@gmail.com>
2024-09-21 08:47:54 +0200

    Comment Model Comment CRUD corrected

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,28 @@
+# blog/forms.py
+from django import forms
+from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Comment
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3}),
+        }
---
+++ django_blog/blog/views.py
@@ -0,0 +1,143 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from .models import Post, Comment
+from .forms import CommentForm
+
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'  # Specify the template for the login page
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+class HomePageView(TemplateView):
+    def register(request):
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Registration successful.')
+                return redirect('login')
+            else:
+                messages.error(request, 'Registration failed. Please check the form.')
+        else:
+            form = CustomUserCreationForm()
+            return render(request, 'register.html', {'form': form})
+            # return HttpResponse redirect
+    
+    def login_view(request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return render(request, 'login.html')
+        
+    def logout_view(request):
+        logout(request)
+        return redirect('login')
+    
+    @login_required
+    def profile(request):
+        if request.method == 'POST':
+            user = request.user
+            user.email = request.POST['email']
+            user.save()
+            messages.success(request, 'Profile updated successfully.')
+        return render(request, 'profile.html', {'user': request.user})
+
+
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+class ListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class DetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class CreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class UpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class DeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+
+@login_required
+def CommentCreateView(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post_id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def CommentUpdateView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect('post_detail', post_id=comment.post.id)
+    if request.method == "POST":
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+
+@login_required
+def CommentDeleteView(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user == comment.author:
+        comment.delete()
+    return redirect('post_detail', post_id=comment.post.id)
