a8638e8503e89816a8f372840bc2ec50960e5a2f
BisaUmutoni <bclaudette636@gmail.com>
2024-09-25 15:37:24 +0200

    Implementing user foolows

--- social_media_api/social_media_api/posts/models.py
+++ social_media_api/social_media_api/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Post (models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
